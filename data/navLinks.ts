e2da5c823da06394975e5e6deb648d5bf8d337d3
Akash Gupta <akashgupta_ce_2021@ltce.in>
2025-10-29 01:11:02 +0530

    todo frontend

--- data/navLinks.ts
+++
@@ -1,6 +0,0 @@
-export const navLinks = [
-  { name: "Home", path: "/" },
-  { name: "About", path: "/about" },
-  { name: "Services", path: "/services" },
-  { name: "Contact", path: "/contact" },
-];
